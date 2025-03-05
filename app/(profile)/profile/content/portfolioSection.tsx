9e35b7f2761513fedca3e36508a0b44aa735216d
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-03-05 14:01:42 +0900

    style: Update portfolioSection.tsx

--- app/(profile)/profile/content/portfolioSection.tsx
+++ app/(profile)/profile/content/portfolioSection.tsx
@@ -6,7 +6,7 @@ const PortfolioSection = () => {
     <div className="flex flex-col">
       <div
         className="flex w-full flex-col gap-3 md:gap-4 md:flex-row md:flex-wrap
-          md:grid md:grid-cols-2 lg:grid-cols-3"
+          md:grid md:grid-cols-3 lg:grid-cols-3 lx:grid-cols-4"
       >
         <PortfolioCard data={PORTFOLIO_DATA.slice(0, 6)} />
       </div>
