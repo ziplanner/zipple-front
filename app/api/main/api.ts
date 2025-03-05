a4ac0a286e15e781e7ffeb10bd5b40b52d9fb27a
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-03-05 14:01:10 +0900

    feat: Update main api.ts

--- app/api/main/api.ts
+++ app/api/main/api.ts
@@ -1,4 +1,4 @@
-import { MAIN_MATCHING } from "../apiUrl";
+import { MAIN_MATCHING, MAIN_PROFILE_DETAIL } from "../apiUrl";
 import axiosInstance from "../axiosInstance";
 
 export const getMainMatching = async () => {
@@ -6,7 +6,20 @@ export const getMainMatching = async () => {
     const response = await axiosInstance.get(MAIN_MATCHING);
     return response.data;
   } catch (error) {
-    console.error("Error fetching user info:", error);
+    console.error("Error fetching main matching info:", error);
+    throw error;
+  }
+};
+
+// 공인 중개사 상세 프로필 API
+export const getAgentProfileDetail = async (agentId: number) => {
+  try {
+    const response = await axiosInstance.get(
+      `${MAIN_PROFILE_DETAIL}/${agentId}`
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching agent profile detail:", error);
     throw error;
   }
 };
