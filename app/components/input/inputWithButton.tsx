52429250bdfad29067c0bda0aa9994a0e3c771ae
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-03-06 13:49:53 +0900

    style: Add mobile style

--- app/components/input/inputWithButton.tsx
+++ app/components/input/inputWithButton.tsx
@@ -27,7 +27,9 @@ const InputWithButton = ({
 }: CustomInputWithButtonProps) => {
   return (
     <div className={`flex flex-col ${className}`}>
-      <label className="text-body2_r text-text_sub4 mb-1">{label}</label>
+      <label className="text-mobile_body2_r md:text-body2_r text-text_sub4 mb-1">
+        {label}
+      </label>
       <div className="flex flex-row gap-2">
         <input
           type={type}
@@ -36,12 +38,12 @@ const InputWithButton = ({
           placeholder={placeholder}
           onChange={onChange}
           className="border border-searchbarborder w-full p-2 focus:outline-none
-            focus:ring-1 focus:ring-selectedoption_pressed transition-all rounded-lg"
+            focus:ring-1 focus:ring-selectedoption_pressed transition-all rounded-lg  text-mobile_body3_r md:text-body2_r"
         />
         <button
           onClick={onButtonClick}
           disabled={disabled}
-          className={`px-4 rounded-lg text-white text-body3_m whitespace-nowrap ${
+          className={`px-4 rounded-lg text-white text-mobile_body3_r md:text-body2_r whitespace-nowrap ${
             disabled
               ? "bg-gray-400 cursor-not-allowed"
               : "bg-primary hover:bg-primary-dark transition-all"
