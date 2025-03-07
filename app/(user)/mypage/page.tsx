7d000b316bdf3fcdb02bf48951dbb7897aa973ac
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-03-07 13:10:24 +0900

    feat: Update page.tsx

--- app/(user)/mypage/page.tsx
+++ app/(user)/mypage/page.tsx
@@ -1,13 +1,24 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import AgentUser from "@/app/components/user/agentUser";
-import GeneralUser from "@/app/components/user/generalUser";
+import Alert from "@/app/components/alert/alert";
 
 const MyPage = () => {
+  const [alertMessage, setAlertMessage] = useState<string | null>(null);
+
+  useEffect(() => {
+    // 회원가입 완료 메시지 확인 후 표시
+    const message = localStorage.getItem("signupSuccessMessage");
+    if (message) {
+      setAlertMessage(message);
+      localStorage.removeItem("signupSuccessMessage");
+    }
+  }, []);
+
   return (
     <div className="w-full">
-      <AgentUser />
-      {/* <GeneralUser /> */}
+      {alertMessage && <Alert message={alertMessage} />} <AgentUser />
     </div>
   );
 };
