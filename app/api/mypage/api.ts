b8406ea51f5d4615bbd044076d6f73bb6504dc07
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-03-11 12:58:41 +0900

    feat: Add api

---
+++ app/api/mypage/api.ts
@@ -0,0 +1,34 @@
+import { MYPAGE_AGENT_PORTFOLIO } from "../apiUrl";
+import axiosInstance from "../axiosInstance";
+
+// 포토폴리오 조회
+export const getMyPortfolio = async (page: number = 1, size: number = 10) => {
+  try {
+    const response = await axiosInstance.get(`${MYPAGE_AGENT_PORTFOLIO}`, {
+      params: { page: page - 1, size },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+};
+
+// 포토폴리오 생성
+export const createMyPortfolio = async (formData: FormData) => {
+  try {
+    const response = await axiosInstance.post(
+      MYPAGE_AGENT_PORTFOLIO,
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error("포트폴리오 생성 중 오류 발생:", error);
+    throw error;
+  }
+};
