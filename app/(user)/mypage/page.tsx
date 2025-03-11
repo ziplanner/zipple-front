c84a0dba919a56e065d8a493e7bbd48d4055fc1a
yeoniii20 <commetoi_yeoni@naver.com>
2025-03-12 00:57:55 +0900

    feat: Add alert modal

--- app/(user)/mypage/page.tsx
+++ app/(user)/mypage/page.tsx
@@ -6,10 +6,15 @@ import AgentUser from "@/app/components/user/agentUser";
 import Alert from "@/app/components/alert/alert";
 import { useUserInfoStore } from "@/app/providers/userStoreProvider";
 import LoadingSpinner from "@/app/components/loading/loadingSpinner";
+import AlertWithBtn from "@/app/components/alert/alertwithBtn";
+import { useRouter } from "next/router";
 
 const MyPage = () => {
-  const [alertMessage, setAlertMessage] = useState<string | null>(null);
+  const router = useRouter();
   const { userInfo } = useUserInfoStore((state) => state);
+
+  const [alertMessage, setAlertMessage] = useState<string | null>(null);
+  const [showSignupAlert, setShowSignupAlert] = useState<boolean>(false);
 
   useEffect(() => {
     const message = localStorage.getItem("signupSuccessMessage");
@@ -17,7 +22,10 @@ const MyPage = () => {
       setAlertMessage(message);
       localStorage.removeItem("signupSuccessMessage");
     }
-  }, []);
+    if (userInfo?.roleName === "미등록") {
+      setShowSignupAlert(true);
+    }
+  }, [userInfo]);
 
   if (!userInfo) {
     return <LoadingSpinner />;
@@ -28,6 +36,15 @@ const MyPage = () => {
       {alertMessage && <Alert message={alertMessage} />}
 
       {userInfo.roleName === "GENERAL" ? <GeneralUser /> : <AgentUser />}
+      {/* 미등록 회원 Alert */}
+      {showSignupAlert && (
+        <AlertWithBtn
+          title="회원가입 필요"
+          message="회원가입이 완료되지 않았습니다. 가입을 완료해주세요."
+          onConfirm={() => router.push("/signup")}
+          confirmText="가입하기"
+        />
+      )}
     </div>
   );
 };
