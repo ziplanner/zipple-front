73edb73c17c8f77a03cf73a4a5a281fc06164656
yeoniii20 <commetoi_yeoni@naver.com>
2025-03-18 23:00:26 +0900

    fix: Edit type

--- app/types/category.ts
+++ app/types/category.ts
@@ -47,25 +47,25 @@ export const CATEGORY_LIST = [
 export const specializationOptions = [
   // 주거용 부동산
   { label: "아파트", value: "아파트" },
-  { label: "주택/다가구", value: "주택" },
-  { label: "빌라/다세대", value: "빌라" },
-  { label: "원룸/투룸", value: "원룸" },
+  { label: "주택/다가구", value: "주택/다가구" },
+  { label: "빌라/다세대", value: "빌라/다세대" },
+  { label: "원룸/투룸", value: "원룸/투룸" },
   { label: "오피스텔", value: "오피스텔" },
 
   // 상업/업무용 부동산
   { label: "상가점포", value: "상가점포" },
-  { label: "빌딩/상업시설", value: "빌딩" },
+  { label: "빌딩/상업시설", value: "빌딩/상업시설" },
   { label: "사무실", value: "사무실" },
-  { label: "공장/창고/지식산업센터", value: "공장" },
+  { label: "공장/창고/지식산업센터", value: "공장/창고/지식산업센터" },
 
   // 개발 및 투자 부동산
-  { label: "재건축/재개발", value: "재건축" },
+  { label: "재건축/재개발", value: "재건축/재개발" },
   { label: "토지", value: "토지" },
-  { label: "병원/요양원", value: "병원" },
-  { label: "경매/분양", value: "경매" },
+  { label: "병원/요양원", value: "병원/요양원" },
+  { label: "경매/분양", value: "경매/분양" },
 
   // 특수 목적 부동산
   { label: "종교시설", value: "종교시설" },
-  { label: "호텔/모텔/펜션", value: "호텔" },
+  { label: "호텔/모텔/펜션", value: "호텔/모텔/펜션" },
   { label: "기타", value: "기타" },
 ];
