14a72b5e01182aa21faced2f904a84ecdb829cf0
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-04-04 10:51:26 +0900

    Update axiosInstance.ts

--- app/api/axiosInstance.ts
+++ app/api/axiosInstance.ts
@@ -25,7 +25,10 @@ axiosInstance.interceptors.response.use(
   async (error) => {
     const originalRequest = error.config;
 
-    if (!error.response || error.response.status === 401) {
+    if (
+      !error.response ||
+      (error.response.status === 401 && !originalRequest._retry)
+    ) {
       originalRequest._retry = true;
 
       try {
